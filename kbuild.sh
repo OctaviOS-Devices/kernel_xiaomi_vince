mozzaru/kernel_port

kbuild.sh: Update kernel building script
Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

kbuild.sh
@@ -1,181 +1,183 @@
 #! /bin/bash
 
-#
 # Copyright (C) 2020 StarLight5234
-# Copyright (C) 2021-22 GhostMaster69-dev
+# Copyright (C) 2021-2023 Unitrix Kernel
 #
 
-export DEVICE="VINCE"
-export TC_PATH="$HOME/clang"
-export ZIP_DIR="$HOME/AnyKernel3"
-export KERNEL_DIR=$(pwd)
-export KBUILD_BUILD_USER="venom"
-export KBUILD_BUILD_HOST="codespace"
-export TZ="Asia/Kolkata"
+DEVICE="xiaomi vince"
+TC_PATH="$HOME/clang"
+COMPILER_NAME="clang"
+LD_NAME="ld.lld"
+CROSS_COMPILE_ARM64="aarch64-linux-gnu-"
+CROSS_COMPILE_ARM32="arm-linux-gnueabi-"
+CMDS="LLVM=1 LLVM_IAS=1"
+DEFCONFIG="vince-perf_defconfig kernelsu.config"
+OUT_DIR="$(pwd)/out"
+KERNEL_IMG="$OUT_DIR/arch/arm64/boot/Image.gz-dtb"
+KBUILD_BUILD_USER="ghostmaster69-dev"
+KBUILD_BUILD_HOST="codespace"
+TZ="Asia/Kolkata"
+ZIP_DIR="$HOME/AnyKernel3"
 
 # Ask Telegram Channel/Chat ID
-if [[ -z ${CHANNEL_ID} ]]; then
+if [ -z "$CHANNEL_ID" ]; then
     echo -n "Plox,Give Me Your TG Channel/Group ID:"
     read -r tg_channel_id
-    CHANNEL_ID="${tg_channel_id}"
+    CHANNEL_ID="$tg_channel_id"
 fi
 
 # Ask Telegram Bot API Token
-if [[ -z ${TELEGRAM_TOKEN} ]]; then
+if [ -z "$TELEGRAM_TOKEN" ]; then
     echo -n "Plox,Give Me Your TG Bot API Token:"
     read -r tg_token
-    TELEGRAM_TOKEN="${tg_token}"
-fi
-
-if [[ -z ${UNITRIX_CHANNEL_ID} ]]; then
-    UNITRIX_CHANNEL_ID=$CHANNEL_ID
+    TELEGRAM_TOKEN="$tg_token"
 fi
 
 # Upload buildlog to group
-tg_erlog()
-{
-	ERLOG=$HOME/build/build${BUILD}.txt
-	curl -F document=@"$ERLOG"  "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" \
-			-F chat_id=$CHANNEL_ID \
-			-F caption="Build ran into errors after $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds, plox check logs"
+function tg_erlog() {
+    ERLOG="$HOME/build/build$BUILD.txt"
+    curl -F document=@"$ERLOG"  "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" \
+	    -F chat_id="$CHANNEL_ID" \
+	    -F caption="Build ran into errors after $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds, plox check logs"
 }
 
 # Upload zip to channel
-tg_pushzip() 
-{
-	FZIP=$ZIP_DIR/$ZIP
-	curl -F document=@"$FZIP"  "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" \
-		   -F chat_id=$UNITRIX_CHANNEL_ID \
-		   -F caption="SHA1: $SHA1"
+function tg_pushzip() {
+    FZIP="$(echo $ZIP_DIR/*.zip)"
+    curl -F document=@"$FZIP"  "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" \
+	    -F chat_id="$CHANNEL_ID" \
+	    -F caption="SHA1: $(cat $ZIP_DIR/*.zip.sha1 | cut -c 1-40)"
 }
 
 # Send Updates
 function tg_sendinfo() {
-	curl -s "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage" \
-		-d "parse_mode=html" \
-		-d text="${1}" \
-		-d chat_id="${CHANNEL_ID}" \
-		-d "disable_web_page_preview=true"
+    curl -s "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage" \
+	    -d "parse_mode=html" \
+	    -d text="$1" \
+	    -d chat_id="$CHANNEL_ID" \
+	    -d "disable_web_page_preview=true"
 }
 
 # Send a sticker
 function start_sticker() {
     curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendSticker" \
-        -d sticker="CAACAgUAAxkBAAMPXvdff5azEK_7peNplS4ywWcagh4AAgwBAALQuClVMBjhY-CopowaBA" \
-        -d chat_id=$CHANNEL_ID
+	    -d sticker="CAACAgUAAxkBAAMPXvdff5azEK_7peNplS4ywWcagh4AAgwBAALQuClVMBjhY-CopowaBA" \
+	    -d chat_id="$CHANNEL_ID"
 }
 
-function error_sticker() {
-    curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendSticker" \
-        -d sticker="$STICKER" \
-        -d chat_id=$CHANNEL_ID
-}
+# clone toolchain
 function clone_tc() {
-if ! [ -d ${TC_PATH} ]; then
-git clone --depth=1 -b master --single-branch https://gitlab.com/GhostMaster69-dev/cosmic-clang.git ${TC_PATH}
-fi
-PATH="${TC_PATH}/bin:$PATH"
-export COMPILER=$(${TC_PATH}/bin/clang -v 2>&1 | grep ' version ' | sed 's/([^)]*)[[:space:]]//' | sed 's/([^)]*)//')
+    if ! [ -d "$TC_PATH" ]; then
+        git clone --depth=1 -b master --single-branch https://gitlab.com/GhostMaster69-dev/cosmic-clang.git $TC_PATH
+    fi
 }
 
+# clone anykernel3
 function clone_anykernel3() {
-	if ! [ -d $ZIP_DIR ]; then
-		git clone -b master https://github.com/AOSP-Silicon/AnyKernel3 $ZIP_DIR
-	fi
+    if ! [ -d $ZIP_DIR ]; then
+        git clone --depth=1 -b vince https://github.com/GhostMaster69-dev/AnyKernel3 $ZIP_DIR
+    fi
 }
 
 # Make Kernel
-build_kernel() {
-DATE=`date`
-BUILD_START=$(date +"%s")
-make LLVM=1 LLVM_IAS=1 defconfig
-make LLVM=1 LLVM_IAS=1 -j$(nproc --all) |& tee -a $HOME/build/build${BUILD}.txt
-BUILD_END=$(date +"%s")
-DIFF=$(($BUILD_END - $BUILD_START))
+function build_kernel() {
+    DATE=$date
+    BUILD_START=$(date +"%s")
+    make ARCH=arm64 CC=$COMPILER_NAME LD=$LD_NAME CROSS_COMPILE=$CROSS_COMPILE_ARM64 CROSS_COMPILE_ARM32=$CROSS_COMPILE_ARM32 $CMDS O=$OUT_DIR $DEFCONFIG
+    make ARCH=arm64 CC=$COMPILER_NAME LD=$LD_NAME CROSS_COMPILE=$CROSS_COMPILE_ARM64 CROSS_COMPILE_ARM32=$CROSS_COMPILE_ARM32 $CMDS O=$OUT_DIR -j$(nproc --all) |& tee -a $HOME/build/build$BUILD.txt
+    BUILD_END=$(date +"%s")
+    DIFF=$(($BUILD_END - $BUILD_START))
 }
 
+# Make flashable zip
 function make_flashable() {
-
-cd $ZIP_DIR
-make clean &>/dev/null
-cp $KERN_IMG $ZIP_DIR
-if [ "$BRANCH" == "test" ]; then
-	make test &>/dev/null
-elif [ "$BRANCH" == "beta" ]; then
-	make beta &>/dev/null
-else
-	make stable &>/dev/null
-fi
-ZIP=$(echo *.zip)
-SHA1=$(cat *.zip.sha1 | cut -c 1-40)
-tg_pushzip
-
+    make -C $ZIP_DIR clean &>/dev/null
+    cp $KERNEL_IMG $ZIP_DIR
+    cp -rf $(find $OUT_DIR -name "*.ko" &>/dev/null) $ZIP_DIR/modules/system/lib/modules &>/dev/null
+    if [ -a $ZIP_DIR/modules/system/lib/modules/*.ko ]; then
+	sed -i "s/do.modules=0/do.modules=1/g" $ZIP_DIR/anykernel.sh
+    else
+	sed -i "s/do.modules=1/do.modules=0/g" $ZIP_DIR/anykernel.sh
+    fi
+    if [ "$BRANCH" = "test" ]; then
+	make LINUX_VERSION="$KERNEL_VERSION" -C $ZIP_DIR test &>/dev/null
+    elif [ "$BRANCH" = "beta" ]; then
+	make LINUX_VERSION="$KERNEL_VERSION" -C $ZIP_DIR beta &>/dev/null
+    else
+	make LINUX_VERSION="$KERNEL_VERSION" -C $ZIP_DIR stable &>/dev/null
+    fi
 }
 
 # Credits: @madeofgreat
 BTXT="$HOME/build/buildno.txt" #BTXT is Build number TeXT
 if ! [ -a "$BTXT" ]; then
-	mkdir $HOME/build
-	touch $HOME/build/buildno.txt
-	echo $RANDOM > $BTXT
+    mkdir $HOME/build
+    touch $HOME/build/buildno.txt
+    echo $RANDOM > $BTXT
 fi
-
 BUILD=$(cat $BTXT)
 BUILD=$(($BUILD + 1))
-echo ${BUILD} > $BTXT
+echo $BUILD > $BTXT
 
-stick=$(($RANDOM % 5))
-
-if [ "$stick" == "0" ]; then
-	STICKER="CAACAgUAAxkBAAMQXvdgEdkCuvPzzQeXML3J6srMN4gAAvIAA3PMoVfqdoREJO6DahoE"
-elif [ "$stick" == "1" ];then
-	STICKER="CAACAgQAAxkBAAMRXveCWisHv4FNMrlAacnmFRWSL0wAAgEBAAJyIUgjtWOZJdyKFpMaBA"
-elif [ "$stick" == "2" ];then
-	STICKER="CAACAgUAAxkBAAMSXveCj7P1y5I5AAGaH2wt2tMCXuqZAAL_AAO-xUFXBB9-5f3MjMsaBA"
-elif [ "$stick" == "3" ];then
-	STICKER="CAACAgUAAxkBAAMTXveDSSQq2q8fGrIvpmJ4kPx8T1AAAhEBAALKhyBVEsDSQXY-jrwaBA"
-elif [ "$stick" == "4" ];then
-	STICKER="CAACAgUAAxkBAAMUXveDrb4guQZSu7mP7ZptE4547PsAAugAA_scAAFXWZ-1a2wWKUcaBA"
+# send stickers if build failed
+sticker=$(($RANDOM % 5))
+if [ "$sticker" = "0" ]; then
+    STICKER="CAACAgUAAxkBAAMQXvdgEdkCuvPzzQeXML3J6srMN4gAAvIAA3PMoVfqdoREJO6DahoE"
+elif [ "$sticker" = "1" ];then
+    STICKER="CAACAgQAAxkBAAMRXveCWisHv4FNMrlAacnmFRWSL0wAAgEBAAJyIUgjtWOZJdyKFpMaBA"
+elif [ "$sticker" = "2" ];then
+    STICKER="CAACAgUAAxkBAAMSXveCj7P1y5I5AAGaH2wt2tMCXuqZAAL_AAO-xUFXBB9-5f3MjMsaBA"
+elif [ "$sticker" = "3" ];then
+    STICKER="CAACAgUAAxkBAAMTXveDSSQq2q8fGrIvpmJ4kPx8T1AAAhEBAALKhyBVEsDSQXY-jrwaBA"
+elif [ "$sticker" = "4" ];then
+    STICKER="CAACAgUAAxkBAAMUXveDrb4guQZSu7mP7ZptE4547PsAAugAA_scAAFXWZ-1a2wWKUcaBA"
 fi
+
+function error_sticker() {
+    curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendSticker" \
+	    -d sticker="$STICKER" \
+	    -d chat_id="$CHANNEL_ID"
+}
 
 # Upload build logs file on telegram channel
 function tg_push_logs() {
-	LOG=$HOME/build/build${BUILD}.txt
-	curl -F document=@"$LOG"  "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" \
-        -F chat_id=$CHANNEL_ID \
-        -F caption="Build Finished after $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds"
+    LOG=$HOME/build/build$BUILD.txt
+    curl -F document=@"$LOG"  "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" \
+	    -F chat_id=$CHANNEL_ID \
+	    -F caption="Build Finished after $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds"
 }
 
-# Start cloning toolchain
+# The magic begins here.
 clone_tc
-
-COMMIT=$(git log --pretty=format:'"%h : %s"' -1)
-BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-KERNEL_DIR=$(pwd)
-KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
-VENDOR_MODULEDIR="$ZIP_DIR/modules/vendor/lib/modules"
-export KERN_VER=$(echo "$(make --no-print-directory kernelversion)")
-
-# Cleaning source
-make mrproper && rm -rf out
-
+PATH="$TC_PATH/bin:$PATH"
+make mrproper &>/dev/null
+if ! [ -d "$OUT_DIR" ]; then
+rm -rf $OUT_DIR
+fi
 start_sticker
-tg_sendinfo "$(echo -e "======= <b>$DEVICE</b> =======\n
-Build-Host   :- <b>$KBUILD_BUILD_HOST</b>
-Build-User   :- <b>$KBUILD_BUILD_USER</b>\n 
-Version      :- <u><b>$KERN_VER</b></u>
-Compiler     :- <i>$COMPILER</i>\n
-on Branch    :- <b>$BRANCH</b>
-Commit       :- <b>$COMMIT</b>\n")"
-
+COMPILER_VERSION="$($COMPILER_NAME --version | head -n 1 | sed 's/[[:space:]](.*).*//'), $($LD_NAME --version | sed 's/[[:space:]](.*)//')"
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+COMMIT_HASH="$(git log --pretty=format:'%h' -1)"
+COMMIT_MESSAGE="$(git log --pretty=format:'%s' -1)"
+KERNEL_VERSION="$(make --no-print-directory kernelversion)"
+tg_sendinfo "$(echo -e "\n
+<b>Device</b>: <code>$DEVICE</code>\n
+<b>Date</b>: <code>$(date +"%d %B %Y")</code>\n
+<b>Linux Version</b>: <code>$KERNEL_VERSION</code>\n
+<b>Username</b>: <code>$KBUILD_BUILD_USER</code>
+<b>Hostname</b>: <code>$KBUILD_BUILD_HOST</code>\n
+<b>Compiler version</b>: <code>$COMPILER_VERSION</code>\n
+<b>Commit Branch</b>: <code>$BRANCH</code>
+<b>Commit Hash</b>: <code>$COMMIT_HASH</code>
+<b>Commit Message</b>: <i>$COMMIT_MESSAGE</i>\n")"
 build_kernel
 clone_anykernel3
-
-# Check if kernel img is there or not and make flashable accordingly
-
-if ! [ -a "$KERN_IMG" ]; then
-	tg_erlog && error_sticker
-	exit 1
+if ! [ -a "$KERNEL_IMG" ]; then
+    tg_erlog
+    error_sticker
+    exit 1
 else
-	tg_push_logs && make_flashable
+    tg_push_logs
+    make_flashable
+    tg_pushzip
 fi

OctaviOS-Devices/kernel_xiaomi_vince

kbuild.sh
@@ -2,180 +2,183 @@
 
 #
 # Copyright (C) 2020 StarLight5234
-# Copyright (C) 2021-22 GhostMaster69-dev
+# Copyright (C) 2021-2023 Unitrix Kernel
 #
 
-export DEVICE="VINCE"
-export TC_PATH="$HOME/clang"
-export ZIP_DIR="$HOME/AnyKernel3"
-export KERNEL_DIR=$(pwd)
-export KBUILD_BUILD_USER="venom"
-export KBUILD_BUILD_HOST="codespace"
-export TZ="Asia/Kolkata"
+DEVICE="xiaomi vince"
+TC_PATH="$HOME/clang"
+COMPILER_NAME="clang"
+LD_NAME="ld.lld"
+CROSS_COMPILE_ARM64="aarch64-linux-gnu-"
+CROSS_COMPILE_ARM32="arm-linux-gnueabi-"
+EXTRA_CMDS="LLVM=1 LLVM_IAS=1"
+DEFCONFIG="vince-perf_defconfig kernelsu.config"
+OUT_DIR="$(pwd)/out"
+KERNEL_IMG="$OUT_DIR/arch/arm64/boot/Image.gz-dtb"
+KBUILD_BUILD_USER="ghostmaster69-dev"
+KBUILD_BUILD_HOST="codespace"
+TZ="Asia/Kolkata"
+ZIP_DIR="$HOME/AnyKernel3"
 
 # Ask Telegram Channel/Chat ID
-if [[ -z ${CHANNEL_ID} ]]; then
+if [ -z "$CHANNEL_ID" ]; then
     echo -n "Plox,Give Me Your TG Channel/Group ID:"
     read -r tg_channel_id
-    CHANNEL_ID="${tg_channel_id}"
+    CHANNEL_ID="$tg_channel_id"
 fi
 
 # Ask Telegram Bot API Token
-if [[ -z ${TELEGRAM_TOKEN} ]]; then
+if [ -z "$TELEGRAM_TOKEN" ]; then
     echo -n "Plox,Give Me Your TG Bot API Token:"
     read -r tg_token
-    TELEGRAM_TOKEN="${tg_token}"
-fi
-
-if [[ -z ${UNITRIX_CHANNEL_ID} ]]; then
-    UNITRIX_CHANNEL_ID=$CHANNEL_ID
+    TELEGRAM_TOKEN="$tg_token"
 fi
 
 # Upload buildlog to group
-tg_erlog()
-{
-	ERLOG=$HOME/build/build${BUILD}.txt
-	curl -F document=@"$ERLOG"  "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" \
-			-F chat_id=$CHANNEL_ID \
-			-F caption="Build ran into errors after $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds, plox check logs"
+function tg_erlog() {
+    ERLOG="$HOME/build/build$BUILD.txt"
+    curl -F document=@"$ERLOG"  "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" \
+	    -F chat_id="$CHANNEL_ID" \
+	    -F caption="Build ran into errors after $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds, plox check logs"
 }
 
 # Upload zip to channel
-tg_pushzip() 
-{
-	FZIP=$ZIP_DIR/$ZIP
-	curl -F document=@"$FZIP"  "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" \
-		   -F chat_id=$UNITRIX_CHANNEL_ID \
-		   -F caption="SHA1: $SHA1"
+function tg_pushzip() {
+    FZIP="$(echo $ZIP_DIR/*.zip)"
+    curl -F document=@"$FZIP"  "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" \
+	    -F chat_id="$CHANNEL_ID" \
+	    -F caption="SHA1: $(cat $ZIP_DIR/*.zip.sha1 | cut -c 1-40)"
 }
 
 # Send Updates
 function tg_sendinfo() {
-	curl -s "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage" \
-		-d "parse_mode=html" \
-		-d text="${1}" \
-		-d chat_id="${CHANNEL_ID}" \
-		-d "disable_web_page_preview=true"
+    curl -s "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage" \
+	    -d "parse_mode=html" \
+	    -d text="$1" \
+	    -d chat_id="$CHANNEL_ID" \
+	    -d "disable_web_page_preview=true"
 }
 
 # Send a sticker
 function start_sticker() {
     curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendSticker" \
-        -d sticker="CAACAgUAAxkBAAMPXvdff5azEK_7peNplS4ywWcagh4AAgwBAALQuClVMBjhY-CopowaBA" \
-        -d chat_id=$CHANNEL_ID
+	    -d sticker="CAACAgUAAxkBAAMPXvdff5azEK_7peNplS4ywWcagh4AAgwBAALQuClVMBjhY-CopowaBA" \
+	    -d chat_id="$CHANNEL_ID"
 }
 
-function error_sticker() {
-    curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendSticker" \
-        -d sticker="$STICKER" \
-        -d chat_id=$CHANNEL_ID
-}
+# clone toolchain
 function clone_tc() {
-if ! [ -d ${TC_PATH} ]; then
-git clone --depth=1 -b master --single-branch https://gitlab.com/GhostMaster69-dev/cosmic-clang.git ${TC_PATH}
-fi
-PATH="${TC_PATH}/bin:$PATH"
-export COMPILER=$(${TC_PATH}/bin/clang -v 2>&1 | grep ' version ' | sed 's/([^)]*)[[:space:]]//' | sed 's/([^)]*)//')
+    if ! [ -d "$TC_PATH" ]; then
+        git clone --depth=1 -b master --single-branch https://gitlab.com/GhostMaster69-dev/cosmic-clang.git $TC_PATH
+    fi
 }
 
+# clone anykernel3
 function clone_anykernel3() {
-	if ! [ -d $ZIP_DIR ]; then
-		git clone -b master https://github.com/AOSP-Silicon/AnyKernel3 $ZIP_DIR
-	fi
+    if ! [ -d $ZIP_DIR ]; then
+        git clone --depth=1 -b vince https://github.com/GhostMaster69-dev/AnyKernel3 $ZIP_DIR
+    fi
 }
 
 # Make Kernel
-build_kernel() {
-DATE=`date`
-BUILD_START=$(date +"%s")
-make LLVM=1 LLVM_IAS=1 defconfig
-make LLVM=1 LLVM_IAS=1 -j$(nproc --all) |& tee -a $HOME/build/build${BUILD}.txt
-BUILD_END=$(date +"%s")
-DIFF=$(($BUILD_END - $BUILD_START))
+function build_kernel() {
+    DATE=$date
+    BUILD_START=$(date +"%s")
+    make ARCH=arm64 CC=$COMPILER_NAME LD=$LD_NAME CROSS_COMPILE=$CROSS_COMPILE_ARM64 CROSS_COMPILE_ARM32=$CROSS_COMPILE_ARM32 $EXTRA_CMDS O=$OUT_DIR $DEFCONFIG
+    make ARCH=arm64 CC=$COMPILER_NAME LD=$LD_NAME CROSS_COMPILE=$CROSS_COMPILE_ARM64 CROSS_COMPILE_ARM32=$CROSS_COMPILE_ARM32 $EXTRA_CMDS O=$OUT_DIR -j$(nproc --all) |& tee -a $HOME/build/build$BUILD.txt
+    BUILD_END=$(date +"%s")
+    DIFF=$(($BUILD_END - $BUILD_START))
 }
 
+# Make flashable zip
 function make_flashable() {
-
-cd $ZIP_DIR
-make clean &>/dev/null
-cp $KERN_IMG $ZIP_DIR
-if [ "$BRANCH" == "test" ]; then
-	make test &>/dev/null
-elif [ "$BRANCH" == "beta" ]; then
-	make beta &>/dev/null
-else
-	make stable &>/dev/null
-fi
-ZIP=$(echo *.zip)
-SHA1=$(cat *.zip.sha1 | cut -c 1-40)
-tg_pushzip
-
+    make -C $ZIP_DIR clean &>/dev/null
+    cp $KERNEL_IMG $ZIP_DIR
+    cp -rf $(find $OUT_DIR -name "*.ko" &>/dev/null) $ZIP_DIR/modules/system/lib/modules &>/dev/null
+    if [ -a $ZIP_DIR/modules/system/lib/modules/*.ko ]; then
+	sed -i "s/do.modules=0/do.modules=1/g" $ZIP_DIR/anykernel.sh
+    else
+	sed -i "s/do.modules=1/do.modules=0/g" $ZIP_DIR/anykernel.sh
+    fi
+    if [ "$BRANCH" = "test" ]; then
+	make LINUX_VERSION="$KERNEL_VERSION" -C $ZIP_DIR test &>/dev/null
+    elif [ "$BRANCH" = "beta" ]; then
+	make LINUX_VERSION="$KERNEL_VERSION" -C $ZIP_DIR beta &>/dev/null
+    else
+	make LINUX_VERSION="$KERNEL_VERSION" -C $ZIP_DIR stable &>/dev/null
+    fi
 }
 
 # Credits: @madeofgreat
 BTXT="$HOME/build/buildno.txt" #BTXT is Build number TeXT
 if ! [ -a "$BTXT" ]; then
-	mkdir $HOME/build
-	touch $HOME/build/buildno.txt
-	echo $RANDOM > $BTXT
+    mkdir $HOME/build
+    touch $HOME/build/buildno.txt
+    echo $RANDOM > $BTXT
 fi
-
 BUILD=$(cat $BTXT)
 BUILD=$(($BUILD + 1))
-echo ${BUILD} > $BTXT
+echo $BUILD > $BTXT
 
-stick=$(($RANDOM % 5))
-
-if [ "$stick" == "0" ]; then
-	STICKER="CAACAgUAAxkBAAMQXvdgEdkCuvPzzQeXML3J6srMN4gAAvIAA3PMoVfqdoREJO6DahoE"
-elif [ "$stick" == "1" ];then
-	STICKER="CAACAgQAAxkBAAMRXveCWisHv4FNMrlAacnmFRWSL0wAAgEBAAJyIUgjtWOZJdyKFpMaBA"
-elif [ "$stick" == "2" ];then
-	STICKER="CAACAgUAAxkBAAMSXveCj7P1y5I5AAGaH2wt2tMCXuqZAAL_AAO-xUFXBB9-5f3MjMsaBA"
-elif [ "$stick" == "3" ];then
-	STICKER="CAACAgUAAxkBAAMTXveDSSQq2q8fGrIvpmJ4kPx8T1AAAhEBAALKhyBVEsDSQXY-jrwaBA"
-elif [ "$stick" == "4" ];then
-	STICKER="CAACAgUAAxkBAAMUXveDrb4guQZSu7mP7ZptE4547PsAAugAA_scAAFXWZ-1a2wWKUcaBA"
+# send stickers if build failed
+sticker=$(($RANDOM % 5))
+if [ "$sticker" = "0" ]; then
+    STICKER="CAACAgUAAxkBAAMQXvdgEdkCuvPzzQeXML3J6srMN4gAAvIAA3PMoVfqdoREJO6DahoE"
+elif [ "$sticker" = "1" ];then
+    STICKER="CAACAgQAAxkBAAMRXveCWisHv4FNMrlAacnmFRWSL0wAAgEBAAJyIUgjtWOZJdyKFpMaBA"
+elif [ "$sticker" = "2" ];then
+    STICKER="CAACAgUAAxkBAAMSXveCj7P1y5I5AAGaH2wt2tMCXuqZAAL_AAO-xUFXBB9-5f3MjMsaBA"
+elif [ "$sticker" = "3" ];then
+    STICKER="CAACAgUAAxkBAAMTXveDSSQq2q8fGrIvpmJ4kPx8T1AAAhEBAALKhyBVEsDSQXY-jrwaBA"
+elif [ "$sticker" = "4" ];then
+    STICKER="CAACAgUAAxkBAAMUXveDrb4guQZSu7mP7ZptE4547PsAAugAA_scAAFXWZ-1a2wWKUcaBA"
 fi
+
+function error_sticker() {
+    curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendSticker" \
+	    -d sticker="$STICKER" \
+	    -d chat_id="$CHANNEL_ID"
+}
 
 # Upload build logs file on telegram channel
 function tg_push_logs() {
-	LOG=$HOME/build/build${BUILD}.txt
-	curl -F document=@"$LOG"  "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" \
-        -F chat_id=$CHANNEL_ID \
-        -F caption="Build Finished after $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds"
+    LOG=$HOME/build/build$BUILD.txt
+    curl -F document=@"$LOG"  "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" \
+	    -F chat_id=$CHANNEL_ID \
+	    -F caption="Build Finished after $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds"
 }
 
-# Start cloning toolchain
+# The magic begins here.
 clone_tc
-
-COMMIT=$(git log --pretty=format:'"%h : %s"' -1)
-BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-KERNEL_DIR=$(pwd)
-KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
-VENDOR_MODULEDIR="$ZIP_DIR/modules/vendor/lib/modules"
-export KERN_VER=$(echo "$(make --no-print-directory kernelversion)")
-
-# Cleaning source
-make mrproper && rm -rf out
-
+PATH="$TC_PATH/bin:$PATH"
+make mrproper &>/dev/null
+if ! [ -d "$OUT_DIR" ]; then
+rm -rf $OUT_DIR
+fi
 start_sticker
-tg_sendinfo "$(echo -e "======= <b>$DEVICE</b> =======\n
-Build-Host   :- <b>$KBUILD_BUILD_HOST</b>
-Build-User   :- <b>$KBUILD_BUILD_USER</b>\n 
-Version      :- <u><b>$KERN_VER</b></u>
-Compiler     :- <i>$COMPILER</i>\n
-on Branch    :- <b>$BRANCH</b>
-Commit       :- <b>$COMMIT</b>\n")"
-
+COMPILER_VERSION="$($COMPILER_NAME --version | head -n 1 | sed 's/[[:space:]](.*).*//'), $($LD_NAME --version | sed 's/[[:space:]](.*)//')"
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+COMMIT_HASH="$(git log --pretty=format:'%h' -1)"
+COMMIT_MESSAGE="$(git log --pretty=format:'%s' -1)"
+KERNEL_VERSION="$(make --no-print-directory kernelversion)"
+tg_sendinfo "$(echo -e "\n
+<b>Device</b>: <code>$DEVICE</code>\n
+<b>Date</b>: <code>$(date +"%d %B %Y")</code>\n
+<b>Linux Version</b>: <code>$KERNEL_VERSION</code>\n
+<b>Username</b>: <code>$KBUILD_BUILD_USER</code>
+<b>Hostname</b>: <code>$KBUILD_BUILD_HOST</code>\n
+<b>Compiler version</b>: <code>$COMPILER_VERSION</code>\n
+<b>Commit Branch</b>: <code>$BRANCH</code>
+<b>Commit Hash</b>: <code>$COMMIT_HASH</code>
+<b>Commit Message</b>: <i>$COMMIT_MESSAGE</i>\n")"
 build_kernel
 clone_anykernel3
-
-# Check if kernel img is there or not and make flashable accordingly
-
-if ! [ -a "$KERN_IMG" ]; then
-	tg_erlog && error_sticker
-	exit 1
+if ! [ -a "$KERNEL_IMG" ]; then
+    tg_erlog
+    error_sticker
+    exit 1
 else
-	tg_push_logs && make_flashable
+    tg_push_logs
+    make_flashable
+    tg_pushzip
 fi